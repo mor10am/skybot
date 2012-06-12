b1cf8336f724524f0d2eb65a95bf9690474b17b6
Morten Amundsen <mortena@tpn.no>
2012-06-12 11:30:42 +0200

    remove testing

--- async.php
+++ async.php
@@ -1,7 +1,5 @@
 <?php
 
-$test = "TzoyNToiU2t5Ym90XFNreXBlXEFzeW5jTWVzc2FnZSI6Nzp7czo5OiJtZXNzYWdlaWQiO3M6NDoiMzQ5MCI7czo0OiJib2R5IjtzOjEwOiJjb3VudGFzeW5jIjtzOjk6InRpbWVzdGFtcCI7czoxMDoiMTMzOTQ5MTY0NyI7czo5OiJza3lwZW5hbWUiO047czo2OiJjaGF0aWQiO3M6NDg6IiNtb3J0ZW5fYW11bmRzZW4vJHN5c2Rldl9za3lib3Q7NGE1YjNjNjQxNDVhNjZiZiI7czo2OiJyZXN1bHQiO2E6MTp7aTowO3M6MTA6ImNvdW50YXN5bmMiO31zOjY6InBsdWdpbiI7czoyNDoiU2t5Ym90XFBsdWdpblxDb3VudEFzeW5jIjt9";
-
 $loader = require_once 'vendor/autoload.php';
 
 use Monolog\Logger;
@@ -26,8 +24,7 @@ $dic['log'] = $log;
 
 $plugincontainer = new \Skybot\PluginContainer($dic);
 
-//$chatmsg = unserialize(base64_decode($argv[1]));
-$chatmsg = unserialize(base64_decode($test));
+$chatmsg = unserialize(base64_decode($argv[1]));
 $chatmsg->dic = $dic;
 
 $class = $chatmsg->plugin;
