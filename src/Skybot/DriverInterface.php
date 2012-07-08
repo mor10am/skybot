9621a7890115ed3f5973ea1aa1cf11f2546f3786
Morten Amundsen <mortena@tpn.no>
2012-07-08 18:00:10 +0200

    add license text where it was missing

--- src/Skybot/DriverInterface.php
+++ src/Skybot/DriverInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+* This file is part of Skybot
+*
+* (c) 2012 Morten Amundsen <mor10am@gmail.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
 namespace Skybot;
 
 use Skybot\Main;
